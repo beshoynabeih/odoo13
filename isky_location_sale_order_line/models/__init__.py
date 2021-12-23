cf32d74490960a629d2e4d8bdca1f6b36d56276f
Beshoy Nabeih <beshoynabeih@gmail.com>
2021-12-23 21:45:45 +0200

    [ADD] location_sale_order_line

--- isky_location_sale_order_line/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import sale
-from . import stock_location
-from . import operation_type
--- isky_location_sale_order_line/models/operation_type.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-
-class OperationType(models.Model):
-    _inherit = 'stock.picking.type'
-
-    default_operation_type = fields.Boolean(help='Default Operation type for sales')
